0948bed5fbcace804115e3bc4a7af160ef2ef7a8
Soham <sohamjani007@gmail.com>
2022-03-23 15:11:09 +0530

    Installed silk and Settings Folder Changes

---
+++ core/urls.py
@@ -0,0 +1,9 @@
+from tempfile import template
+from django.urls import path
+from django.views.generic import TemplateView
+
+
+# URLConf
+urlpatterns = [
+    path('', TemplateView.as_view(template_name='core/index.html'))
+]
