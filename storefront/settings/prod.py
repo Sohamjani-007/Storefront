3350c0be9d786a3af7ecb391365305655a23a0c1
Soham <sohamjani007@gmail.com>
2022-03-24 19:20:33 +0530

    Starbuy Web

--- storefront/settings/prod.py
+++ storefront/settings/prod.py
@@ -27,7 +27,8 @@ CACHES = {
     }
 }
 
-EMAIL_HOST = os.environ['MAILGUN_SMTP_SERVER']
-EMAIL_HOST_USER = os.environ['MAILGUN_SMTP_LOGIN']
-EMAIL_HOST_PASSWORD = os.environ['MAILGUN_SMTP_PASSWORD']
-EMAIL_PORT = os.environ['MAILGUN_SMTP_PORT']
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'localhost'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_PORT = 2525
